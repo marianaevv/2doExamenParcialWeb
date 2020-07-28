5889bff6f950022408383faa43ba68b31a00b80d
Marianavv <mariana2v@hotmail.com>
2020-07-28 18:52:03 -0500

    2doParcial

--- server.js
+++ server.js
@@ -2,29 +2,29 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const app = express();
-app.use(bodyParser.json())
+
 // settings
 app.set('port', process.env.PORT || 5000);
-//app.use(express.bodyParser());
-//app.use(app.router);
-//app.use(express.json());
-
+app.use(express.json());
+app.use(bodyParser.json())
 
 //routes
+//GET
 app.get('/', (req,res) =>{
     res.send("<h1>You are on the homepage</h1>");
 })
+//POST
 app.post('/post', (req,res) =>{
     console.log(req.body);
    res.send(`Welcome ${req.body.user}`);
 })
+//DELETE
 app.delete('/delete', (req,res) =>{
     console.log(req.body);
      res.send(`delete : true`);
  })
-
+//PUT
  app.put('/put/:id',(req,res)=>{
      res.send(`Task ${req.params.id} has been updated`);
  })
